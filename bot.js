f78a6bc2839c07cab7b8f7f2c3caa42e07d50c4c
Atlas <164876872+RenathoM@users.noreply.github.com>
2026-01-19 15:02:34 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5330,7 +5330,7 @@ async function getRobloxAvatarUrl(userId) {
 
       // For now, show instruction
       await interaction.reply({
-        content: '📸 Please attach the proof image to your next message in this channel.\n\nAfter you send the image, the proof will be automatically forwarded to the records channel.',
+        content: '📸 Please attach the proof image to your next message in this channel.\n\n| > Take a screenshot of the screen where you are making the trade, send the image via any DM, go to the image page, copy the image link, and paste the image link into the input box of the "Upload Proof Image" button.',
         ephemeral: false
       });
 
